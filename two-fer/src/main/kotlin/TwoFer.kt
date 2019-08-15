78723bee2de43f917ee9e2be5d618dee1e2b68d4
dalerlarsen <dalerlarsen@gmail.com>
2019-08-15 09:43:11 -0400

    two-fer - updated

--- two-fer/src/main/kotlin/TwoFer.kt
+++ two-fer/src/main/kotlin/TwoFer.kt
@@ -1,3 +1,3 @@
-fun twofer(name: String? = null) : String {
-    return "One for ${name ?: "you"}, one for me."
+fun twofer(name: String = "you") : String {
+    return "One for $name, one for me."
 }
