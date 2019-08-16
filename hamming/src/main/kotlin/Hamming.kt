154ecce37ed9e18718a4db2c7220cc7dc636f435
dalerlarsen <dalerlarsen@gmail.com>
2019-08-16 13:21:36 -0400

    hamming - functional, use foldIndexed

--- hamming/src/main/kotlin/Hamming.kt
+++ hamming/src/main/kotlin/Hamming.kt
@@ -1,18 +1,12 @@
 import java.lang.IllegalArgumentException
 
-class Hamming {
-    companion object {
-        fun compute(s1: String, s2: String) : Int {
-            if (s1.length != s2.length) {
-                throw IllegalArgumentException("left and right strands must be of equal length.")
-            }
-            var numDifferent = 0
-            s1.forEachIndexed { index, c1 ->
-                if (c1 != s2[index]) {
-                    numDifferent++
-                }
-            }
-            return numDifferent
+object Hamming {
+    fun compute(s1: String, s2: String): Int {
+        if (s1.length != s2.length) {
+            throw IllegalArgumentException("left and right strands must be of equal length.")
+        }
+        return s1.foldIndexed(0) { index, acc, c ->
+            if (c != s2[index]) acc + 1 else acc
         }
     }
 }
