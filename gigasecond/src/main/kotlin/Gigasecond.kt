80d46807eb5962481a15ee5e14b6b20e24c661c6
dalerlarsen <dalerlarsen@gmail.com>
2019-08-19 15:02:10 -0400

    Gigasecond - remove commented code

--- gigasecond/src/main/kotlin/Gigasecond.kt
+++ gigasecond/src/main/kotlin/Gigasecond.kt
@@ -1,10 +1,8 @@
 import java.time.LocalDate
 import java.time.LocalDateTime
-import java.time.temporal.ChronoUnit
 
 class Gigasecond(currentDateTime: LocalDateTime) {
     constructor(currentDate: LocalDate) : this(currentDate.atStartOfDay())
 
-//    val date: LocalDateTime = currentDateTime.plus(1000000000, ChronoUnit.SECONDS)
     val date: LocalDateTime = currentDateTime.plusSeconds(1_000_000_000)
 }
