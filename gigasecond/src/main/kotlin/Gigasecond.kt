350d239774a45cc99ba99eb14ac910ef589bbfa9
dalerlarsen <dalerlarsen@gmail.com>
2019-08-19 14:34:58 -0400

    Gigasecond - take2, use plusSeconds

--- gigasecond/src/main/kotlin/Gigasecond.kt
+++ gigasecond/src/main/kotlin/Gigasecond.kt
@@ -5,5 +5,6 @@ import java.time.temporal.ChronoUnit
 class Gigasecond(currentDateTime: LocalDateTime) {
     constructor(currentDate: LocalDate) : this(currentDate.atStartOfDay())
 
-    val date: LocalDateTime = currentDateTime.plus(1000000000, ChronoUnit.SECONDS)
+//    val date: LocalDateTime = currentDateTime.plus(1000000000, ChronoUnit.SECONDS)
+    val date: LocalDateTime = currentDateTime.plusSeconds(1_000_000_000)
 }
