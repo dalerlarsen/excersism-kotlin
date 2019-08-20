656ff4dd45a96b0da9bc16585136388296a18d3d
dalerlarsen <dalerlarsen@gmail.com>
2019-08-20 07:36:46 -0400

    ScrabbleScore - map, sum with correct values

--- scrabble-score/src/main/kotlin/ScrabbleScore.kt
+++ scrabble-score/src/main/kotlin/ScrabbleScore.kt
@@ -3,12 +3,12 @@ object ScrabbleScore {
         return input.map {
             when {
                 Regex("[AEIOULNRST]", RegexOption.IGNORE_CASE).matches(it.toString()) -> 1
-                Regex("[DG]", RegexOption.IGNORE_CASE).matches(it.toString()) -> 1
-                Regex("[BCMP]", RegexOption.IGNORE_CASE).matches(it.toString()) -> 1
-                Regex("[FHVWY]", RegexOption.IGNORE_CASE).matches(it.toString()) -> 1
-                Regex("[K]", RegexOption.IGNORE_CASE).matches(it.toString()) -> 1
-                Regex("[JX]", RegexOption.IGNORE_CASE).matches(it.toString()) -> 1
-                Regex("[QZ]", RegexOption.IGNORE_CASE).matches(it.toString()) -> 1
+                Regex("[DG]", RegexOption.IGNORE_CASE).matches(it.toString()) -> 2
+                Regex("[BCMP]", RegexOption.IGNORE_CASE).matches(it.toString()) -> 3
+                Regex("[FHVWY]", RegexOption.IGNORE_CASE).matches(it.toString()) -> 4
+                Regex("[K]", RegexOption.IGNORE_CASE).matches(it.toString()) -> 5
+                Regex("[JX]", RegexOption.IGNORE_CASE).matches(it.toString()) -> 8
+                Regex("[QZ]", RegexOption.IGNORE_CASE).matches(it.toString()) -> 10
                 else -> 0
             }
         }.sum()
