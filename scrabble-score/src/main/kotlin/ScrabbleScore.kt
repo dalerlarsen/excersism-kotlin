fa98a1077f038ebf224cd2b599811cf3ce15199b
Dale Larsen <dale.larsen@pinnsg.com>
2019-08-20 05:50:50 -0400

    ScrabbleScore - map, sum

--- scrabble-score/src/main/kotlin/ScrabbleScore.kt
+++ scrabble-score/src/main/kotlin/ScrabbleScore.kt
@@ -1,5 +1,16 @@
 object ScrabbleScore {
     fun scoreWord(input: String) : Int {
-        return 0
+        return input.map {
+            when {
+                Regex("[AEIOULNRST]", RegexOption.IGNORE_CASE).matches(it.toString()) -> 1
+                Regex("[DG]", RegexOption.IGNORE_CASE).matches(it.toString()) -> 1
+                Regex("[BCMP]", RegexOption.IGNORE_CASE).matches(it.toString()) -> 1
+                Regex("[FHVWY]", RegexOption.IGNORE_CASE).matches(it.toString()) -> 1
+                Regex("[K]", RegexOption.IGNORE_CASE).matches(it.toString()) -> 1
+                Regex("[JX]", RegexOption.IGNORE_CASE).matches(it.toString()) -> 1
+                Regex("[QZ]", RegexOption.IGNORE_CASE).matches(it.toString()) -> 1
+                else -> 0
+            }
+        }.sum()
     }
 }
