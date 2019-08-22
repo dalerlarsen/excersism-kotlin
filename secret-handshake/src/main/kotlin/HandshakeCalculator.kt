8353211de0c6c5037aa8bf7883760817ec064a74
dalerlarsen <dalerlarsen@gmail.com>
2019-08-22 14:41:33 -0400

    secret handshake - almost

--- secret-handshake/src/main/kotlin/HandshakeCalculator.kt
+++ secret-handshake/src/main/kotlin/HandshakeCalculator.kt
@@ -1,5 +1,6 @@
 object HandshakeCalculator {
     fun calculateHandshake(decimal: Int) : List<Signal> {
-        return listOf(Signal.WINK)
+        val secrets = Signal.values().filter { decimal and it.code == it.code }
+        return (if (decimal and 16 == 16) secrets.reversed() else secrets)
     }
 }
