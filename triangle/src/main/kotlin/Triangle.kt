449f02d5dfdf01d58a3c913d43bdeb822a94eb04
dalerlarsen <dalerlarsen@gmail.com>
2019-08-26 08:36:10 -0400

    triangle - initial commit

---
+++ triangle/src/main/kotlin/Triangle.kt
@@ -0,0 +1,8 @@
+class Triangle(s1: Double, s2: Double, s3: Double) {
+    constructor(i1: Int, i2: Int, i3: Int)  : this(i1.toDouble(), i2.toDouble(), i3.toDouble())
+
+    val isEquilateral: Boolean = true
+    val isIsosceles: Boolean = true
+    val isScalene: Boolean = true
+
+}
