48bdf2a4b8cc8b9ef3e4e898a6b59d8282c4efea
dalerlarsen <dalerlarsen@gmail.com>
2019-08-27 14:13:49 -0400

    triangle - iteration 2

--- triangle/src/main/kotlin/Triangle.kt
+++ triangle/src/main/kotlin/Triangle.kt
@@ -1,20 +1,22 @@
-import java.lang.IllegalArgumentException
-
 class Triangle(private val s1: Double, private val s2: Double, private val s3: Double) {
     constructor(i1: Int, i2: Int, i3: Int)  : this(i1.toDouble(), i2.toDouble(), i3.toDouble())
 
-    val isEquilateral: Boolean = isTriangle() && s1 == s2 && s2 == s3 && s1 == s3
+    private val uniqueSides : Int = listOf(s1, s2, s3).distinct().size
 
-    val isIsosceles: Boolean = isTriangle() && (s1 == s2 || s2 == s3 || s1 == s3)
+    val isDegenerate: Boolean =
+        (s1 != 0.0 && s2 != 0.0 && s3 != 0.0) &&
+        (s1 + s2 == s3 || s1 + s3 == s2 || s2 + s3 == s1)
 
-    val isScalene: Boolean = isTriangle() && !isEquilateral && !isIsosceles
+    val isEquilateral: Boolean = isTriangle() && uniqueSides == 1
 
-    val isDegenerate: Boolean = isTriangle() &&  (s1 + s2 == s3 || s1 + s3 == s2 || s2 + s3 == s1)
+    val isIsosceles: Boolean = isTriangle() && uniqueSides <= 2
 
-    private fun isTriangle() : Boolean =
-        when {
-            s1 == 0.0 || s2 == 0.0|| s3 == 0.0 -> throw IllegalArgumentException("All sides must have length > 0")
-            s1 + s2 < s3 || s1 + s3 < s2 || s2 + s3 < s1 -> throw IllegalArgumentException("Sum of 2 sides must be greater than or equal to third side")
-            else -> true
+    val isScalene: Boolean = isTriangle() && uniqueSides == 3
+
+    private fun isTriangle() : Boolean {
+        if (!isDegenerate) {
+            require((s1 + s2 > s3 && s1 + s3 > s2 && s2 + s3 > s1))
         }
+        return true
+    }
 }
